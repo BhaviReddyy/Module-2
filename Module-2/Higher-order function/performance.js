b9fc055e72e1e0172a9c7b0b316411eb3479fe66
bhavya <bhavyareddy506@gmail.com>
2025-11-20 18:23:39 +0530

    performance

---
+++ Module-2/Higher-order function/performance.js
@@ -0,0 +1,39 @@
+function evaluateEmployees(employees) {
+    const filtered = employees.filter(emp => emp.tasksCompleted > 5);
+    const mapped = filtered.map(emp => {
+        let performance = "";
+
+        if (emp.rating > 4.5) {
+            performance = "Excellent";
+        } else if (emp.rating >= 3 && emp.rating <= 4.5) {
+            performance = "Good";
+        } else {
+            performance = "Needs Improvement";
+        }
+
+        return {
+            name: emp.name,
+            performance: performance
+        };
+    });
+    const priority = {
+        "Excellent": 3,
+        "Good": 2,
+        "Needs Improvement": 1
+    };
+
+    const sorted = mapped.sort((a, b) => {
+        return priority[b.performance] - priority[a.performance];
+    });
+
+    return sorted;
+}
+const employees = [
+    { name: "Bhavya", tasksCompleted: 8, rating: 4.7 },
+    { name: "Chandu", tasksCompleted: 4, rating: 4.0 },
+    { name: "Reethu", tasksCompleted: 6, rating: 3.5 },
+    { name: "Rishi", tasksCompleted: 10, rating: 4.9 },
+    { name: "Bhavani", tasksCompleted: 7, rating: 2.8 }
+];
+
+console.log(evaluateEmployees(employees));
